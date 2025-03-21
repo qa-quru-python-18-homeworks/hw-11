f1d447e13a8055ef41f90a74a48ee08e51eca682
Waldemar G <vladdigen@icloud.com>
2025-03-21 20:22:24 +0400

    Add registration form page and tests with browser setup (high level steps implementation)

--- pages/registration_form_page.py
+++ pages/registration_form_page.py
@@ -4,24 +4,54 @@ from selene.support.shared.jquery_style import s
 
 
 class RegistrationFormPage:
+    def fill_form(
+            self,
+            first_name,
+            last_name,
+            email,
+            gender,
+            mobile,
+            date_of_birth,
+            subjects,
+            hobbies,
+            picture,
+            address,
+            state,
+            city
+    ):
+        self.__open_page() \
+            .__set_first_name(first_name) \
+            .__set_last_name(last_name) \
+            .__set_email(email) \
+            .__select_gender(gender) \
+            .__set_mobile_number(mobile) \
+            .__set_birth_date(*date_of_birth) \
+            .__add_subjects(subjects) \
+            .__select_hobbies(hobbies) \
+            .__upload_picture(picture) \
+            .__set_current_address(address) \
+            .set_state(state) \
+            .__set_city(city) \
+            .__submit()
+        return self
 
-    def open_page(self):
+    def __open_page(self):
         browser.open('https://demoqa.com/automation-practice-form')
         return self
 
-    def set_first_name(self, value):
+    def __set_first_name(self, value):
         s('#firstName').type(value)
         return self
 
-    def set_last_name(self, value):
+    def __set_last_name(self, value):
         s('#lastName').type(value)
         return self
 
-    def set_email(self, value):
+    def __set_email(self, value):
         s('#userEmail').type(value)
         return self
 
-    def select_gender(self, gender):
+    def __select_gender(self, gender):
         gender_map = {
             'Male': 'label[for="gender-radio-1"]',
             'Female': 'label[for="gender-radio-2"]',
@@ -30,11 +60,11 @@ class RegistrationFormPage:
         s(gender_map[gender]).click()
         return self
 
-    def set_mobile_number(self, value):
+    def __set_mobile_number(self, value):
         s('#userNumber').type(value)
         return self
 
-    def set_birth_date(self, day: int, month: int, year: int):
+    def __set_birth_date(self, day: int, month: int, year: int):
         s('#dateOfBirthInput').click()
         s('.react-datepicker__month-select').element(f'option[value="{month}"]').click()
         s('.react-datepicker__year-select').element(f'option[value="{year}"]').click()
@@ -42,12 +72,12 @@ class RegistrationFormPage:
         s(f'.react-datepicker__day--0{day_str}:not(.react-datepicker__day--outside-month)').click()
         return self
 
-    def add_subjects(self, subjects):
+    def __add_subjects(self, subjects):
         for subject in subjects:
             s('#subjectsInput').type(subject).press_enter()
         return self
 
-    def select_hobbies(self, hobbies):
+    def __select_hobbies(self, hobbies):
         hobby_map = {
             'Sports': 'label[for="hobbies-checkbox-1"]',
             'Reading': 'label[for="hobbies-checkbox-2"]',
@@ -57,11 +87,11 @@ class RegistrationFormPage:
             s(hobby_map[hobby]).click()
         return self
 
-    def upload_picture(self, file_path):
+    def __upload_picture(self, file_path):
         s('#uploadPicture').send_keys(file_path)
         return self
 
-    def set_current_address(self, value):
+    def __set_current_address(self, value):
         s('#currentAddress').type(value)
         return self
 
@@ -70,12 +100,12 @@ class RegistrationFormPage:
         s('#react-select-3-input').type(state_name).press_enter()
         return self
 
-    def set_city(self, city_name):
+    def __set_city(self, city_name):
         s('#city').click()
         s('#react-select-4-input').type(city_name).press_enter()
         return self
 
-    def submit(self):
+    def __submit(self):
         s('#submit').click()
         return self
 
