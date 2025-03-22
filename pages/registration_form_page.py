fea214b32df4c966a4e59cedd7d06047a4d07ce7
Waldemar G <vladdigen@icloud.com>
2025-03-22 12:51:45 +0400

    Add Allure steps; Add utilities for Allure attaching screenshots, logs, HTML, and videos in tests

--- pages/registration_form_page.py
+++ pages/registration_form_page.py
@@ -1,110 +1,155 @@
+import allure
 from selene import have, be, command
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
 
+from utils import attach
+
 
 class RegistrationFormPage:
 
+    @allure.step("Открыть страницу с формой регистрации")
     def open_page(self):
-        browser.open('https://demoqa.com/automation-practice-form')
+        browser.open("https://demoqa.com/automation-practice-form")
+        browser.driver.execute_script('document.querySelector("#fixedban").remove();')
+        browser.driver.execute_script('document.querySelector("footer").remove();')
+        attach.add_screenshot(browser)
+        attach.add_html(browser)
+        attach.add_logs(browser)
         return self
 
+    @allure.step("Заполнить поле 'First Name'")
     def set_first_name(self, value):
-        s('#firstName').type(value)
+        s("#firstName").type(value)
+        attach.add_screenshot(browser)
         return self
 
+    @allure.step("Заполнить поле 'Last Name'")
     def set_last_name(self, value):
-        s('#lastName').type(value)
+        s("#lastName").type(value)
+        attach.add_screenshot(browser)
         return self
 
+    @allure.step("Заполнить поле 'Email'")
     def set_email(self, value):
-        s('#userEmail').type(value)
+        s("#userEmail").type(value)
+        attach.add_screenshot(browser)
         return self
 
+    @allure.step("Выбрать пол")
     def select_gender(self, gender):
         gender_map = {
-            'Male': 'label[for="gender-radio-1"]',
-            'Female': 'label[for="gender-radio-2"]',
-            'Other': 'label[for="gender-radio-3"]'
+            "Male": 'label[for="gender-radio-1"]',
+            "Female": 'label[for="gender-radio-2"]',
+            "Other": 'label[for="gender-radio-3"]',
         }
         s(gender_map[gender]).click()
+        attach.add_screenshot(browser)
         return self
 
+    @allure.step("Заполнить поле 'Mobile'")
     def set_mobile_number(self, value):
-        s('#userNumber').type(value)
+        s("#userNumber").type(value)
+        attach.add_screenshot(browser)
         return self
 
+    @allure.step("Установить дату рождения")
     def set_birth_date(self, day: int, month: int, year: int):
-        s('#dateOfBirthInput').click()
-        s('.react-datepicker__month-select').element(f'option[value="{month}"]').click()
-        s('.react-datepicker__year-select').element(f'option[value="{year}"]').click()
-        day_str = f'{day:02d}'
-        s(f'.react-datepicker__day--0{day_str}:not(.react-datepicker__day--outside-month)').click()
+        s("#dateOfBirthInput").click()
+        s(".react-datepicker__month-select").element(f'option[value="{month}"]').click()
+        s(".react-datepicker__year-select").element(f'option[value="{year}"]').click()
+        day_str = f"{day:02d}"
+        s(
+            f".react-datepicker__day--0{day_str}:not(.react-datepicker__day--outside-month)"
+        ).click()
+        attach.add_screenshot(browser)
         return self
 
+    @allure.step("Добавить предметы")
     def add_subjects(self, subjects):
         for subject in subjects:
-            s('#subjectsInput').type(subject).press_enter()
+            s("#subjectsInput").type(subject).press_enter()
+        attach.add_screenshot(browser)
         return self
 
+    @allure.step("Выбрать хобби")
     def select_hobbies(self, hobbies):
         hobby_map = {
-            'Sports': 'label[for="hobbies-checkbox-1"]',
-            'Reading': 'label[for="hobbies-checkbox-2"]',
-            'Music': 'label[for="hobbies-checkbox-3"]'
+            "Sports": 'label[for="hobbies-checkbox-1"]',
+            "Reading": 'label[for="hobbies-checkbox-2"]',
+            "Music": 'label[for="hobbies-checkbox-3"]',
         }
         for hobby in hobbies:
             s(hobby_map[hobby]).click()
+        attach.add_screenshot(browser)
         return self
 
+    @allure.step("Загрузить картинку")
     def upload_picture(self, file_path):
-        s('#uploadPicture').send_keys(file_path)
+        s("#uploadPicture").send_keys(file_path)
+        attach.add_screenshot(browser)
         return self
 
+    @allure.step("Заполнить поле 'Current Address'")
     def set_current_address(self, value):
-        s('#currentAddress').type(value)
+        s("#currentAddress").type(value)
+        attach.add_screenshot(browser)
         return self
 
+    @allure.step("Выбрать штат")
     def set_state(self, state_name):
-        s('#state').click()
-        s('#react-select-3-input').type(state_name).press_enter()
+        s("#state").click()
+        s("#react-select-3-input").type(state_name).press_enter()
+        attach.add_screenshot(browser)
         return self
 
+    @allure.step("Выбрать город")
     def set_city(self, city_name):
-        s('#city').click()
-        s('#react-select-4-input').type(city_name).press_enter()
+        s("#city").click()
+        s("#react-select-4-input").type(city_name).press_enter()
+        attach.add_screenshot(browser)
         return self
 
+    @allure.step("Нажать кнопку 'Submit'")
     def submit(self):
-        s('#submit').click()
+        attach.add_html(browser)
+        attach.add_logs(browser)
+        s("#submit").click()
+        attach.add_screenshot(browser)
         return self
 
+    @allure.step("Проверить, что форма была успешно отправлена")
     def should_have_submitted(
-            self,
-            student_name,
-            student_email,
-            gender,
-            mobile,
-            date_of_birth,
-            subjects,
-            hobbies,
-            picture,
-            address,
-            state_and_city
+        self,
+        student_name,
+        student_email,
+        gender,
+        mobile,
+        date_of_birth,
+        subjects,
+        hobbies,
+        picture,
+        address,
+        state_and_city,
     ):
-        s('.modal-content').should(be.visible)
-        s('table tbody tr:nth-of-type(1) td:nth-of-type(2)').should(have.text(student_name))
-        s('table tbody tr:nth-of-type(2) td:nth-of-type(2)').should(have.text(student_email))
-        s('table tbody tr:nth-of-type(3) td:nth-of-type(2)').should(have.text(gender))
-        s('table tbody tr:nth-of-type(4) td:nth-of-type(2)').should(have.text(mobile))
-        s('table tbody tr:nth-of-type(5) td:nth-of-type(2)').should(have.text(date_of_birth))
-        s('table tbody tr:nth-of-type(6) td:nth-of-type(2)').should(have.text(subjects))
-        s('table tbody tr:nth-of-type(7) td:nth-of-type(2)').should(have.text(hobbies))
-        s('table tbody tr:nth-of-type(8) td:nth-of-type(2)').should(have.text(picture))
-        s('table tbody tr:nth-of-type(9) td:nth-of-type(2)').should(have.text(address))
-        s('table tbody tr:nth-of-type(10) td:nth-of-type(2)').should(have.text(state_and_city))
+        attach.add_screenshot(browser)
+        attach.add_html(browser)
+        attach.add_logs(browser)
+        s(".modal-content").should(be.visible)
+        s("table tbody tr:nth-of-type(1) td:nth-of-type(2)").should(have.text(student_name))
+        s("table tbody tr:nth-of-type(2) td:nth-of-type(2)").should(have.text(student_email))
+        s("table tbody tr:nth-of-type(3) td:nth-of-type(2)").should(have.text(gender))
+        s("table tbody tr:nth-of-type(4) td:nth-of-type(2)").should(have.text(mobile))
+        s("table tbody tr:nth-of-type(5) td:nth-of-type(2)").should(have.text(date_of_birth))
+        s("table tbody tr:nth-of-type(6) td:nth-of-type(2)").should(have.text(subjects))
+        s("table tbody tr:nth-of-type(7) td:nth-of-type(2)").should(have.text(hobbies))
+        s("table tbody tr:nth-of-type(8) td:nth-of-type(2)").should(have.text(picture))
+        s("table tbody tr:nth-of-type(9) td:nth-of-type(2)").should(have.text(address))
+        s("table tbody tr:nth-of-type(10) td:nth-of-type(2)").should(have.text(state_and_city))
         return self
 
+    @allure.step("Закрыть модальное окно")
     def close_modal(self):
-        s('#closeLargeModal').perform(command.js.click)
+        s("#closeLargeModal").perform(command.js.click)
+        attach.add_screenshot(browser)
         return self
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,43 @@
+import allure
 import pytest
+import os
+
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from utils.attach import add_video
+from dotenv import load_dotenv
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
+
+
+@pytest.fixture(scope="session", autouse=True)
 def setup_browser():
-    browser.driver.maximize_window()
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {"enableVNC": True, "enableVideo": True},
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options,
+    )
+
+    browser.config.driver = driver
+
+    allure.step("Развернуть браузер во весь экран")(
+        lambda: browser.driver.maximize_window()
+    )()
     yield
-    browser.quit()
+    allure.step("Закрыть браузер")(lambda: browser.quit())()
+    add_video(browser)
